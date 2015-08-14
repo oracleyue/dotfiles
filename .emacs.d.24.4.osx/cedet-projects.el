4519a136e08ba10b93de31b8fb473a78ce219ab6
Zuogon Yue <zuogong.yue@uni.lu>
2015-08-14 21:09:12 +0200

    regular update

--- .emacs.d.24.4.osx/cedet-projects.el
+++
@@ -1,7 +0,0 @@
-(ede-cpp-root-project "project_root"
-                      :file "/Users/oracleyue/tmp/eigen/src/third.cc"
-                      :include-path '("../include"
-                                      "/usr/local/include/eigen3"
-                                      )
-                      :system-include-path '("~/include")
-                      )
--- .emacs.d.24.4.osx/elpa/pos-tip-20150318.813/pos-tip-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; pos-tip-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil nil ("pos-tip.el") (21966 8927 824318 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; pos-tip-autoloads.el ends here
