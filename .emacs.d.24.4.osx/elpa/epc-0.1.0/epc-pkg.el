8b0ee4c230724eb48966c505f656a768e5f55ded
Zuogon Yue <zuogong.yue@uni.lu>
2016-06-03 19:59:00 +0200

    regular update

---
+++ .emacs.d.24.4.osx/elpa/epc-0.1.0/epc-pkg.el
@@ -0,0 +1,8 @@
+(define-package "epc" "0.1.0" "A RPC stack for the Emacs Lisp"
+  '((concurrent "0.3.1")
+    (ctable "0.1.0"))
+  :url "https://github.com/kiwanami/emacs-epc" :keywords
+  '("lisp" "rpc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ .emacs.d.24.4.osx/elpa/python-environment-0.0.2/python-environment-pkg.el
@@ -0,0 +1,7 @@
+(define-package "python-environment" "0.0.2" "virtualenv API for Emacs Lisp"
+  '((deferred "0.3.1"))
+  :keywords
+  '("applications" "tools"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
