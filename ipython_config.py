e07af2cbe107d3ee54f537fd5d2b76c6dac46506
Zuogon Yue <zuogong.yue@uni.lu>
2016-06-23 17:58:00 +0200

    startup window size config

---
+++ ipython_config.py
@@ -0,0 +1,11 @@
+# Config for IPython. To use, place it in ~/.ipython/profile_default
+c = get_config()
+
+c.IPythonWidget.font_size = 12
+#c.IPythonWidget.font_family = 'DejaVu Sans Mono'
+c.IPythonWidget.font_family = 'Menlo'
+
+c.IPythonWidget.height = 36
+c.IPythonWidget.width= 96
+
+
