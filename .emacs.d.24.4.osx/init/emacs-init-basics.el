5077d9bf557b51e8bb8c098a069349fb5aa0664a
Zuogon Yue <zuogong.yue@uni.lu>
2016-12-09 17:10:29 +0100

    regular update

--- .emacs.d.24.4.osx/init/emacs-init-basics.el
+++ .emacs.d.24.4.osx/init/emacs-init-basics.el
@@ -107,12 +107,12 @@
 ;; To use dark theme and enabled in terminal, use "set-variable" in .emacs
 ;;(setq frame-background-mode (quote dark))
 ;;!! (setq solarized-termcolors 256)   ;distort colors; do NOT enable
-;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized-theme")
-;(load-theme 'solarized t)
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/solarized-theme")
+(load-theme 'solarized t)
 
 ;; 3) using oracleyue's theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'ymonokai t)
+;(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+;(load-theme 'ymonokai t)
 
 ;; fringe setting (right-only); bug: cause linum-mode to destory the auto-complete popup menu
 ;(fringe-mode '(0 . nil))
@@ -132,36 +132,36 @@
                                :foreground "#F8F8F2")))
 ;; use setq-default to set it for /all/ modes
 (setq-default mode-line-format
-  (list
+   (list
+    " "
     ;; default part
     "%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification
     ;mode-line-buffer-identification
-    
+    "  "
     ;; the buffer name; the file name as a tool tip (default: 'face 'font-lock-keyword-face)
     '(:eval (propertize "%b " 'face 'mode-line-buffer-name-face-y
         'help-echo (buffer-file-name)))
-    
+    "  "
     ;; line and column (default: 'face 'font-lock-type-face)
     "(" ;; '%02' to set to 2 chars at least; prevents flickering
       (propertize "%02l" 'face 'mode-line-linum-face-y) ","
       (propertize "%02c") 
     ") "
-
+    "  "
     ;; relative position, size of file (default: 'face 'font-lock-constant-face)
     "["
     (propertize "%p") ;; % above top
     "/"
     (propertize "%I") ;; size
     "] "
-
+    "         "
     ;; the current major mode for the buffer (default: 'face 'font-lock-string-face)
     "["
 
     '(:eval (propertize "%m"
               'help-echo buffer-file-coding-system))
     "] "
-
-
+    "      "
     "[" ;; insert vs overwrite mode, input-method in a tooltip
     '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
                         'face
@@ -183,14 +183,14 @@
                              'face 'font-lock-type-face
                              'help-echo "Buffer is read-only"))))  
     "] "
-
+    "                     "
     ;; add the time, with the date and the emacs uptime in the tooltip
-    "  --"
+    "  ---"
     '(:eval (propertize (format-time-string "%H:%M")
               'help-echo
               (concat (format-time-string "%c; ")
                       (emacs-uptime "Uptime:%hh"))))
-    "--"
+    "---"
     
     ;; i don't want to see minor-modes; but if you want, uncomment this:
     ;; minor-mode-alist  ;; list of minor modes
@@ -216,7 +216,7 @@
 ;(add-hook 'text-mode-hook 'y-variable-width-text-mode)
 
 ;; set cursor type; default "box"
-;(setq-default cursor-type 'bar) 
+(setq-default cursor-type 'bar) 
 
 ;; font size adjustment
 ;; C-x C-0 : return to default size
