51686d211c144d14b7203af7fab7d1f2eb1175eb
Zuogon Yue <zuogong.yue@uni.lu>
2017-08-18 16:45:37 +0200

    update emacs config from mac

--- _emacs.d.25.1.osx/init/install-init-packages.el
+++ _emacs.d.25.1.osx/init/install-init-packages.el
@@ -1,61 +1,68 @@
 ;; List of Required Packages
-(defvar custom/packages
-  '(auctex
-    auto-complete
-    auto-complete-c-headers
-    auto-complete-clang
-    bash-completion
-    company
-    company-c-headers
-    company-jedi
-    company-quickhelp
-    csv-mode
-    dash
-    emmet-mode
-    ess
-    expand-region
-    flymake-easy
-    flymake-google-cpplint
-    function-args
-    golden-ratio
-    google-c-style
-    helm
-    helm-core
-    helm-gtags
-    helm-projectile
-    helm-swoop
-    iedit
-    jedi
-    jedi-core
-    js2-mode
-    julia-mode
-    key-combo
-    magit
-    markdown-mode
-    matlab-mode
-    multiple-cursors
-    popup
-    pos-tip
-    projectile
-    smartparens
-    undo-tree
-    xcscope
-    yasnippet
-    zeal-at-point) "Default packages")
+(setq custom/packages
+      '(auctex
+        auto-complete
+        auto-complete-c-headers
+        auto-complete-clang
+        bash-completion
+        company
+        company-c-headers
+        company-jedi
+        company-quickhelp
+        csv-mode
+        dash
+        emmet-mode
+        ess
+        expand-region
+        flymake-easy
+        flymake-google-cpplint
+        function-args
+        golden-ratio
+        google-c-style
+        helm
+        helm-core
+        helm-gtags
+        helm-projectile
+        helm-swoop
+        iedit
+        jedi
+        jedi-core
+        js2-mode
+        julia-mode
+        key-combo
+        magit
+        markdown-mode
+        matlab-mode
+        multiple-cursors
+        popup
+        pos-tip
+        projectile
+        smartparens
+        undo-tree
+        xcscope
+        yasnippet
+        zeal-at-point))
 
 
 ;; Function to Install Packages
-(require 'cl)  ;; common-lisp functions used below
 (defun custom/packages-installed-p ()
-  (loop for pkg in custom/packages
-        when (not (package-installed-p pkg)) do (return nil)
-        finally (return t)))
-
-;; Installation
-(unless (custom/packages-installed-p)
-  (when (not package-archive-contents)
-    (message "%s" "Refreshing package database...")
-    (package-refresh-contents)
+  (catch 'exit
     (dolist (pkg custom/packages)
-      (when (not (package-installed-p pkg))
-        (package-install pkg)))))
+      (unless (package-installed-p pkg)
+        (throw 'exit nil)))
+    (throw 'exit t)))
+
+;; Perform Installation
+(unless (custom/packages-installed-p)
+  ;; list pkgs to be installed
+  (message "\n%s" "Refreshing package database...")
+  (message "----------------------")
+  (dolist (pkg custom/packages)
+    (unless (package-installed-p pkg)
+      (message "+ %s" pkg)))
+  (message "----------------------\n")
+  ;; install pkgs
+  (package-refresh-contents)
+  (dolist (pkg custom/packages)
+    (unless (package-installed-p pkg)
+      (package-install pkg))))
