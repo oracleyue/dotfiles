dbea7a98040c1b3106b49b69f5a86a79858cad68
oracleyue <oracleyue@gmail.com>
2017-08-24 01:53:40 +0200

    update emacs config from mac

--- scripts/gitpull-emacs.sh
+++ scripts/gitpull-emacs.sh
@@ -8,16 +8,17 @@
 
 
 # essential bash settings
-sync='/usr/bin/rsync -rlptD -P --delete'
+sync='/usr/bin/rsync -rlptD -P --delete --exclude=.DS_Store'
 repopath=$HOME'/Workspace/gitrepo/dotfiles'
 
 # push updates to github.com
 cd $repopath && git pull && cd ~
 
 # rsync .emacs and .emacs.d from github repo
-$sync $repopath/_emacs.25.1.osx ~/.emacs
-$sync $repopath/_emacs.25.1.osx/init ~/.emacs.d/
-$sync $repopath/_emacs.25.1.osx/git ~/.emacs.d/
-$sync $repopath/_emacs.25.1.osx/themes ~/.emacs.d/
-$sync $repopath/_emacs.25.1.osx/snippets ~/.emacs.d/
-$sync $repopath/_emacs.25.1.osx/default-css ~/.emacs.d/
+# $sync $repopath/_emacs.25.1.osx ~/.emacs
+$sync $repopath/_emacs.d.25.1.osx/init ~/.emacs.d/
+$sync --exclude="clang-complete" --exclude="*.pyc" \
+      $repopath/_emacs.d.25.1.osx/git ~/.emacs.d/
+$sync $repopath/_emacs.d.25.1.osx/themes ~/.emacs.d/
+$sync $repopath/_emacs.d.25.1.osx/snippets ~/.emacs.d/
+$sync $repopath/_emacs.d.25.1.osx/default-css ~/.emacs.d/
--- scripts/gitup-emacs.sh
+++ scripts/gitup-emacs.sh
@@ -12,9 +12,10 @@ sync='/usr/bin/rsync -rlptD -P --delete --exclude=.DS_Store'
 repopath=$HOME'/Workspace/gitrepo/dotfiles'
 
 # rsync .emacs and .emacs.d to the local git repo
-$sync ~/.emacs $repopath/_emacs.25.1.osx
+# $sync ~/.emacs $repopath/_emacs.25.1.osx
 $sync ~/.emacs.d/init $repopath/_emacs.d.25.1.osx/
-$sync --exclude="clang-complete-async/clang-complete" ~/.emacs.d/git $repopath/_emacs.d.25.1.osx/
+$sync --exclude="clang-complete" --exclude="*.pyc" \
+      ~/.emacs.d/git $repopath/_emacs.d.25.1.osx/
 $sync ~/.emacs.d/themes $repopath/_emacs.d.25.1.osx/
 $sync ~/.emacs.d/snippets $repopath/_emacs.d.25.1.osx/
 $sync ~/.emacs.d/default-css $repopath/_emacs.d.25.1.osx/
