8b9ee0aef21bb7bd8c07c9952d598218e13bb783
Zuogon Yue <zuogong.yue@uni.lu>
2017-10-10 15:41:14 +0200

    update emacs config from mac

--- _emacs.d.25.1.osx/init/styles/customize-modeline.el
+++ _emacs.d.25.1.osx/init/styles/customize-modeline.el
@@ -1,169 +1,96 @@
-;;
-;; Use /smart-mode-line/
-;;
-;; (note: require /smart-mode-line/ and /smart-mode-line-powerline-theme/)
-
-;; (defun y:use-smart-mode-line ()
-;;   (setq sml/no-confirm-load-theme t)
-;;   (setq sml/theme 'powerline)
-;;   (sml/setup))
-
+;; ================================================================
+;; Mode-Line Customizations
+;; ================================================================
 
 ;;
-;; User customized mode-line
+;; Customize mode-line
 ;;
 (defun y:customize-mode-line ()
-  (if (eq 'monokai (car custom-enabled-themes))
-      (progn
-        ;; define the colors only for the monokai theme
-        (make-face 'mode-line-linum-face-y)
-        (make-face 'mode-line-buffer-name-face-y)
-        (make-face 'mode-line-plain-face-y)
-        (set-face-attribute 'mode-line-linum-face-y nil
-                            :foreground "#66D9EF")
-        (set-face-attribute 'mode-line-buffer-name-face-y nil
-                            :foreground "#A6E22E"
-                            :bold t)
-        (set-face-attribute 'mode-line-plain-face-y nil
-                            :foreground "#F8F8F2")
+  (make-face 'y/mode-line-rownum-face)
+  (make-face 'y/mode-line-buffer-name-face)
+  (make-face 'y/mode-line-plain-face)
 
-        ;; set default mode line for monokai theme
-        (unless window-system        ;; in terminal
-          (set-face-attribute 'mode-line nil
-                              :background "color-238"))
-        (setq-default mode-line-format
-                      (list
-                       ;; default part
-                       "%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification
+  (unless (eq 'solarized (car custom-enabled-themes))
+    (set-face-attribute 'y/mode-line-buffer-name-face nil
+                        :foreground
+                        (face-foreground 'font-lock-function-name-face))
+    (set-face-attribute 'y/mode-line-rownum-face nil
+                        :foreground
+                        (face-foreground 'font-lock-constant-face)))
 
-                       ;; the buffer name; the file name as a tool tip
-                       ;;   (default: 'face 'font-lock-keyword-face)
-                       '(:eval (propertize "%b " 'face 'mode-line-buffer-name-face-y
-                                           'help-echo (buffer-file-name)))
-
-                       ;; line and column (default: 'face 'font-lock-type-face)
-                       "(" ;; '%02' to set to 2 chars at least; prevents flickering
-                       (propertize "%02l" 'face 'mode-line-linum-face-y) ","
-                       (propertize "%02c")
-                       ") "
-
-                       ;; relative position, size of file
-                       ;;   (default: 'face 'font-lock-constant-face)
-                       "["
-                       (propertize "%p") ;; % above top
-                       "/"
-                       (propertize "%I") ;; size
-                       "] "
-
-                       ;; the current major mode for the buffer
-                       ;;  (default: 'face 'font-lock-string-face)
-                       "["
-                       '(:eval (propertize (if (listp mode-name)
-                                               (mapconcat 'identity (cdr mode-name) "/")
-                                             mode-name)
-                                           'help-echo buffer-file-coding-system))
-                       "] "
-
-                       "[" ;; insert vs overwrite mode, input-method in a tooltip
-                       '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                           'face
-                                           (if overwrite-mode 'font-lock-preprocessor-face
-                                             'mode-line-plain-face-y)
-                                        ;'font-lock-preprocessor-face
-                                           'help-echo (concat "Buffer is in "
-                                                              (if overwrite-mode "overwrite" "insert") " mode")))
-
-                       ;; was this buffer modified since the last save?
-                       '(:eval (when (buffer-modified-p)
-                                 (concat ","  (propertize "Mod"
-                                                          'face 'font-lock-warning-face
-                                                          'help-echo "Buffer has been modified"))))
-
-                       ;; is this buffer read-only?
-                       '(:eval (when buffer-read-only
-                                 (concat ","  (propertize "RO"
-                                                          'face 'font-lock-type-face
-                                                          'help-echo "Buffer is read-only"))))
-                       "] "
-
-                       ;; add the time, with the date and the emacs uptime in the tooltip
-                       "  ---"
-                       '(:eval (propertize (format-time-string "%H:%M")
-                                           'help-echo
-                                           (concat (format-time-string "%c; ")
-                                                   (emacs-uptime "Uptime:%hh"))))
-                       "---"
-
-                       ;; i don't want to see minor-modes; but if you want, uncomment this:
-                       ;; minor-mode-alist  ;; list of minor modes
-
-                       ;; "%-" ;; fill with '-'
-                       )))
-    ;; set default mode line for other themes
+  ;; configure box style
+  (when (eq 'atom-one-dark (car custom-enabled-themes))
     (set-face-attribute 'mode-line nil
-                        :box '(:line-width 1 :style released-button))
-    (setq-default mode-line-format
-                  (list
-                   ;; default part
-                   "%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification
+                        :box '(:line-width 1 :style released-button)))
 
-                   ;; the buffer name; the file name as a tool tip
-                   '(:eval (propertize "%b " 'face 'bold
-                                       'help-echo (buffer-file-name)))
+  ;; amendments for terminal
+  (when (and (not (daemonp)) (not window-system))
+    (set-face-attribute 'mode-line nil
+                        :background "color-238"))
 
-                   ;; line and column
-                   "("
-                   (propertize "%02l") ","
-                   (propertize "%02c")
-                   ") "
+  ;; format default mode line
+  (setq-default mode-line-format
+                (list
+                 ;; default part
+                 "%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification
 
-                   ;; relative position, size of file
-                   "["
-                   (propertize "%p") ;; % above top
-                   "/"
-                   (propertize "%I") ;; size
-                   "] "
+                 ;; the buffer name; the file name as a tool tip
+                 '(:eval (propertize "%b " 'face 'y/mode-line-buffer-name-face
+                                     'help-echo (buffer-file-name)))
 
-                   ;; the current major mode for the buffer
-                   "["
-                   '(:eval (propertize (if (listp mode-name)
-                                           (mapconcat 'identity (cdr mode-name) "/")
-                                         mode-name)
-                                       'help-echo buffer-file-coding-system))
-                   "] "
-                   ;; "      "
-                   "[" ;; insert vs overwrite mode, input-method in a tooltip
-                   '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
-                                       'face
-                                       (if overwrite-mode 'font-lock-warning-face nil)
-                                       'help-echo (concat "Buffer is in "
-                                                          (if overwrite-mode "overwrite" "insert") " mode")))
+                 ;; line and column
+                 "("
+                 (propertize "%02l" 'face 'y/mode-line-rownum-face)
+                 ","
+                 (propertize "%02c")
+                 ") "
 
-                   ;; was this buffer modified since the last save?
-                   '(:eval (when (buffer-modified-p)
-                             (concat ","  (propertize "Mod" 'face 'error
-                                                      'help-echo "Buffer has been modified"))))
+                 ;; relative position, size of file
+                 "["
+                 (propertize "%p") ;; % above top
+                 "/"
+                 (propertize "%I") ;; size
+                 "] "
 
-                   ;; is this buffer read-only?
-                   '(:eval (when buffer-read-only
-                             (concat ","  (propertize "RO" 'face 'font-lock-type-face
-                                                      'help-echo "Buffer is read-only"))))
-                   "] "
+                 ;; the current major mode for the buffer
+                 "["
+                 '(:eval (propertize (if (listp mode-name)
+                                         (mapconcat 'identity (cdr mode-name) "/")
+                                       mode-name)
+                                     'help-echo buffer-file-coding-system))
+                 "] "
+                 ;; "      "
+                 "[" ;; insert vs overwrite mode, input-method in a tooltip
+                 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+                                     'face
+                                     (if overwrite-mode 'font-lock-warning-face nil)
+                                     'help-echo (concat "Buffer is in "
+                                                        (if overwrite-mode "overwrite" "insert") " mode")))
 
-                   ;; add the time, with the date and the emacs uptime in the tooltip
-                   "  ---"
-                   '(:eval (propertize (format-time-string "%H:%M")
-                                       'help-echo
-                                       (concat (format-time-string "%c; ")
-                                               (emacs-uptime "Uptime:%hh"))))
-                   "---"
-                   ))))
+                 ;; was this buffer modified since the last save?
+                 '(:eval (when (buffer-modified-p)
+                           (concat ","  (propertize "Mod" 'face 'error
+                                                    'help-echo "Buffer has been modified"))))
+
+                 ;; is this buffer read-only?
+                 '(:eval (when buffer-read-only
+                           (concat ","  (propertize "RO" 'face 'font-lock-type-face
+                                                    'help-echo "Buffer is read-only"))))
+                 "] "
+
+                 ;; add the time, with the date and the emacs uptime in the tooltip
+                 "  ---"
+                 '(:eval (propertize (format-time-string "%H:%M")
+                                     'help-echo
+                                     (concat (format-time-string "%c; ")
+                                             (emacs-uptime "Uptime:%hh"))))
+                 "---"
+                 )))
 
 ;;
 ;; Interface
 ;;
 (defun y:setup-mode-line ()
-  ;; (y:use-smart-mode-line)
   (y:customize-mode-line))
 
 
