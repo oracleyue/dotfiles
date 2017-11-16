5917fef99a003c9da248b4dd5ee18eb1c7c7f17f
Zuogong Yue <zuogong.yue@uni.lu>
2017-11-16 21:27:54 +0100

    update emacs config from mac

---
+++ _emacs.d.25.1.osx/snippets/latex-mode/auto/.emacs.el
@@ -0,0 +1,34 @@
+(TeX-add-style-hook
+ ".emacs"
+ (lambda ()
+   (TeX-add-to-alist 'LaTeX-provided-class-options
+                     '(("beamer" "xcolor={dvipsnames}" "aspectratio=169")))
+   (add-to-list 'LaTeX-verbatim-environments-local "semiverbatim")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (TeX-run-style-hooks
+    "latex2e"
+    "zyueBeamerTheme"
+    "beamer"
+    "beamer10"
+    "latexsym"
+    "amsmath"
+    "amssymb"
+    "amsfonts"
+    "arydshln"
+    "mathrsfs"
+    "mathtools"
+    "upgreek"
+    "graphicx"
+    "subcaption"
+    "tikz"
+    "algorithm"
+    "algpseudocode"
+    "appendixnumberbeamer"))
+ :latex)
+
