d035f7f0845e8684ae3e91875ab28ed05b8d7147
Zuogong Yue <zuogong.yue@uni.lu>
2017-11-17 15:59:14 +0100

    update emacs config from mac

--- _emacs.d.25.1.osx/init/emacs-init-theme.el
+++ _emacs.d.25.1.osx/init/emacs-init-theme.el
@@ -49,7 +49,7 @@
         ((not (daemonp))
          (if (display-graphic-p)
              (load-theme 'solarized t)            ;; GUI
-           (load-theme 'monokai t)))              ;; terminal
+           (load-theme 'Amelie t)))              ;; terminal
         ;; servers (use daemon)
         ((string-equal "main" (daemonp))          ;; server: main
          (load-theme 'solarized t))
