8d932b4b93f10fa23cc8dc7bc25ae93913518431
Zuogong Yue <zuogong.yue@uni.lu>
2018-05-28 16:44:45 +0200

    update emacs config from mac

--- OSX/bin/gitup-dotfiles.sh
+++ OSX/bin/gitup-dotfiles.sh
@@ -50,8 +50,8 @@ $sync ~/.vimperator/colors/oracleyue-dark.vimp \
 
 # Sublime Text
 sublpath=$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
-cp *.sublime-keymap $repopath/editors/sublime-text/
-cp *.sublime-settings $repopath/editors/sublime-text/
+cp $sublpath/*.sublime-keymap $repopath/editors/sublime-text/
+cp $sublpath/*.sublime-settings $repopath/editors/sublime-text/
 
 
 # ===========================================
