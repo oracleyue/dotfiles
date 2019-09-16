f00065a37df0e3941fc51620d8214ad274bd3582
oracleyue <oracleyue@gmail.com>
2019-09-16 13:43:10 +1000

    add olivetti-mode for distraction-free writing

--- _emacs.d/init/init-lang.el
+++ _emacs.d/init/init-lang.el
@@ -4,14 +4,27 @@
 
 
 ;; ---------------------------------------------
+;; major mode for distraction-free writing
+;; ---------------------------------------------
+(use-package olivetti
+  :demand
+  :hook ((olivetti-mode . (lambda () (auto-fill-mode -1)))
+         (olivetti-mode . hide-mode-line-mode))
+  :init
+  (setq olivetti-body-width 0.65)
+  :config
+  ;; hide modeline
+  (use-package hide-mode-line)
+  ;; aliasing
+  (defalias 'writing-mode 'olivetti-mode))
+
+;; ---------------------------------------------
 ;; major mode for /VimScript/ (e.g. ".vimrc")
 ;; ---------------------------------------------
 (use-package vimrc-mode
-  :ensure nil
   :load-path "~/.emacs.d/git"
   :config
   (add-to-list 'auto-mode-alist '("\\.vim\\(rc\\)?\\'" . vimrc-mode)))
-
 
 ;; ---------------------------------------------
 ;; major mode for /AppleScript/
@@ -19,12 +32,10 @@
 (use-package apples-mode
   :mode "\\.\\(applescri\\|sc\\)pt\\'")
 
-
 ;; ---------------------------------------------
 ;; major mode for /YAML/ (*.yml)
 ;; ---------------------------------------------
 (use-package yaml-mode
-  :ensure nil
   :load-path "~/.emacs.d/git"
   :init
   (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode)))
