fa192257a52f398b6a93688cddab85e923ce8f14
oracleyue <oracleyue@gmail.com>
2023-12-29 14:24:17 +0800

    update nvim lsp for matlab

--- OSX/user_nvim/user/init.lua
+++ OSX/user_nvim/user/init.lua
@@ -1,13 +1,26 @@
 return {
   -- Set global colorscheme to use
-  -- colorscheme = "ayu",
+  -- colorscheme = "ayu",  -- require plugin "ayu-theme/ayu-vim"
+
+  -- LSP
+  lsp = {
+    servers = { 
+      'matlab_ls', 
+    },
+    config = {
+      matlab_ls = {
+        single_file_support = false;
+        indexWorkspace = true,
+        installPath = '/Applications/MATLAB_R2022b.app',
+      },
+    },
+  },
 
   -- Advanced configs for Neovide and tty
   polish = function()
     if vim.g.neovide then
-      vim.cmd[[colorscheme ayu]]  -- require plugin "ayu-theme/ayu-vim"
-    else
-      vim.cmd[[colorscheme astromars]]
+      -- vim.cmd[[colorscheme astromars]]
+      vim.cmd[[colorscheme ayu]]
     end
   end,
 }
